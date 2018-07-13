066c352392ec1d21beb1a8b67a856afe5caccfe9
Giang <nguyengiangsc96@gmail.com>
2018-07-13 16:06:01 +0700

    Chapter 4: Rails-flavored Ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
+  scope "(:locale)", locale: /en|vi/ do
+    root to: "static_pages#home"
+  end
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
 end
